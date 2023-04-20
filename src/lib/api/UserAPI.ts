3e32563acc2a0fe698c5170c035a22190c191e64
raymosun <sunraysfs@gmail.com>
2023-04-20 12:26:45 -0700

    Implement user handle routes

--- src/lib/api/UserAPI.ts
+++ src/lib/api/UserAPI.ts
@@ -1,6 +1,11 @@
 /* eslint-disable import/prefer-default-export */
 import { config } from '@/lib';
-import type { GetCurrentUserResponse, PrivateProfile } from '@/lib/types/apiResponses';
+import type {
+  GetCurrentUserResponse,
+  GetUserResponse,
+  PrivateProfile,
+} from '@/lib/types/apiResponses';
+import { PublicProfile } from '@/lib/types/apiResponses';
 import axios from 'axios';
 
 /**
@@ -12,6 +17,24 @@ export const getCurrentUser = async (token: string): Promise<PrivateProfile> => 
   const requestUrl = `${config.api.baseUrl}${config.api.endpoints.user.user}`;
 
   const response = await axios.get<GetCurrentUserResponse>(requestUrl, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+  return response.data.user;
+};
+
+/**
+ * Get specified user's public profile
+ * @param token Authorization bearer token
+ * @param handle Handle of user to retrieve
+ * @returns User's public profile
+ */
+export const getUserByHandle = async (token: string, handle: string): Promise<PublicProfile> => {
+  const requestUrl = `${config.api.baseUrl}${config.api.endpoints.user.handle}/${handle}`;
+
+  const response = await axios.get<GetUserResponse>(requestUrl, {
     headers: {
       Authorization: `Bearer ${token}`,
     },
