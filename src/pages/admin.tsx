3607ee4e723de458e45316f3aa09b9eb557c5125
Faris Ashai <farisashai@gmail.com>
2023-04-19 18:26:43 -0700

    Fix links

--- src/pages/admin.tsx
+++ src/pages/admin.tsx
@@ -1,8 +1,6 @@
 import { config } from '@/lib';
-import { EventManager } from '@/lib/managers';
-import { CookieService } from '@/lib/services';
-import type { PrivateProfile } from '@/lib/types/apiResponses';
-import { CookieType } from '@/lib/types/enums';
+import withAccessType from '@/lib/hoc/withAccessType';
+import { PermissionService } from '@/lib/services';
 import { GetServerSideProps, NextPage } from 'next';
 
 const AdminPage: NextPage = () => {
@@ -15,37 +13,14 @@ const AdminPage: NextPage = () => {
 
 export default AdminPage;
 
-export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
-  const userCookie = CookieService.getServerCookie(CookieType.USER, { req, res });
-
-  if (!userCookie) {
-    return {
-      redirect: {
-        destination: config.loginRoute,
-        permanent: false,
-      },
-    };
-  }
-
-  const user: PrivateProfile = JSON.parse(userCookie);
-
-  const token = CookieService.getServerCookie(CookieType.ACCESS_TOKEN, { req, res });
-
-  if (!token) {
-    return {
-      redirect: {
-        destination: config.loginRoute,
-        permanent: false,
-      },
-    };
-  }
-
-  const events = await EventManager.getAllEvents({ token });
-
+const getServerSidePropsFunc: GetServerSideProps = async () => {
   return {
-    props: {
-      user,
-      events,
-    },
+    props: {},
   };
 };
+
+export const getServerSideProps = withAccessType(
+  getServerSidePropsFunc,
+  PermissionService.canViewAdminPage(),
+  config.homeRoute
+);
