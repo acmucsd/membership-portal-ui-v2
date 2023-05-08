7c29fb6467626a99912669b3870a69ac5e3e1b59
raymosun <sunraysfs@gmail.com>
2023-05-08 04:37:31 -0700

    Initial profile page design

--- src/pages/u/[handle].tsx
+++ src/pages/u/[handle].tsx
@@ -4,6 +4,9 @@ import withAccessType from '@/lib/hoc/withAccessType';
 import { CookieService, PermissionService } from '@/lib/services';
 import type { PublicProfile } from '@/lib/types/apiResponses';
 import { CookieType } from '@/lib/types/enums';
+import defaultProfilePicture from '@/public/assets/profile-pics/adorable0.png';
+import styles from '@/styles/pages/u.module.scss';
+import Image from 'next/image';
 import type { GetServerSideProps } from 'next/types';
 
 interface UserProfilePageProps {
@@ -11,7 +14,41 @@ interface UserProfilePageProps {
 }
 
 const UserProfilePage = ({ user }: UserProfilePageProps) => {
-  return <pre>{JSON.stringify(user, null, 2)}</pre>;
+  // return <pre>{JSON.stringify(user, null, 2)}</pre>;
+  return (
+    <div className={styles.profilePage}>
+      <div className={styles.cardWrapper}>
+        <div className={styles.banner} />
+        <div className={styles.profileCard}>
+          <div className={styles.cardHalf}>
+            <div className={styles.horizontal}>
+              <Image
+                src={user.profilePicture ?? defaultProfilePicture}
+                alt="Profile Picture"
+                className={styles.profilePic}
+              />
+              <div style={{ marginLeft: '1rem' }}>
+                <h1>{`${user.firstName} ${user.lastName}`}</h1>
+                <h3>@{user.handle}</h3>
+                <h2>Rank Here</h2>
+              </div>
+            </div>
+            <div className={styles.iconBox}>icon icon icon icon</div>
+          </div>
+          <div className={styles.cardHalf}>
+            <h2>Points</h2>
+            <h2>Class of {user.graduationYear}</h2>
+            <h2>{user.major}</h2>
+          </div>
+        </div>
+      </div>
+
+      <div className={styles.about}>
+        <h1>About me</h1>
+        <p>{user.bio ?? <i>Nothing here...</i>}</p>
+      </div>
+    </div>
+  );
 };
 
 export default UserProfilePage;
