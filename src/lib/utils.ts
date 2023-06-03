b21c57f27b4c883eca2037e3080def43d62a0d31
Trevor Kwan <trevorkw7@gmail.com>
2023-06-03 11:35:55 -0700

    Remove extraneous type

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -27,9 +27,3 @@ export const getMessagesFromError = (errBody: CustomErrorBody): string[] => {
 
   return errBody.errors.map(err => getAllErrMessages(err)).flat();
 };
-
-// Define general type for useWindowSize hook, which includes width and height
-export interface Size {
-  width: number | undefined;
-  height: number | undefined;
-}
