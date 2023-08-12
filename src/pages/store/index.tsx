fce50b737900cce9e67b27ab627265f35ae0db2a
Sean <ussa.say@gmail.com>
2023-08-12 15:44:36 -0700

    Get collections from store API

--- src/pages/store/index.tsx
+++ src/pages/store/index.tsx
@@ -1,15 +1,18 @@
 import { HelpModal, Hero, Navbar } from '@/components/store';
+import { StoreAPI } from '@/lib/api';
 import withAccessType from '@/lib/hoc/withAccessType';
-import { PermissionService } from '@/lib/services';
-import { PrivateProfile } from '@/lib/types/apiResponses';
+import { CookieService, PermissionService } from '@/lib/services';
+import { PrivateProfile, PublicMerchCollection } from '@/lib/types/apiResponses';
+import { CookieType } from '@/lib/types/enums';
 import styles from '@/styles/pages/store/index.module.scss';
 import { GetServerSideProps } from 'next';
 import { useState } from 'react';
 
 interface HomePageProps {
   user: PrivateProfile;
+  collections: PublicMerchCollection[];
 }
-const StoreHomePage = ({ user: { credits } }: HomePageProps) => {
+const StoreHomePage = ({ user: { credits }, collections }: HomePageProps) => {
   const [helpOpen, setHelpOpen] = useState(false);
   return (
     <>
@@ -18,16 +21,28 @@ const StoreHomePage = ({ user: { credits } }: HomePageProps) => {
       </div>
       <Hero onHelp={() => setHelpOpen(true)} />
       <HelpModal open={helpOpen} onClose={() => setHelpOpen(false)} />
-      <div className={styles.container} />
+      <div className={styles.container}>
+        {collections.map(collection => (
+          <pre key={collection.uuid}>{JSON.stringify(collection, null, 2)}</pre>
+        ))}
+      </div>
     </>
   );
 };
 
 export default StoreHomePage;
 
-const getServerSidePropsFunc: GetServerSideProps = async () => ({
-  props: {},
-});
+const getServerSidePropsFunc: GetServerSideProps = async ({ req, res }) => {
+  const AUTH_TOKEN = CookieService.getServerCookie(CookieType.ACCESS_TOKEN, { req, res });
+
+  const collections = await StoreAPI.getAllCollections(AUTH_TOKEN);
+
+  return {
+    props: {
+      collections,
+    },
+  };
+};
 
 export const getServerSideProps = withAccessType(
   getServerSidePropsFunc,
