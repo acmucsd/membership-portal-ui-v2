e7d31c7a9a4d8931c5a4bd87bbf4851588aa2eb6
Sean <ussa.say@gmail.com>
2023-08-18 14:09:58 -0700

    Show costs under items

--- src/components/store/CollectionSlider/index.tsx
+++ src/components/store/CollectionSlider/index.tsx
@@ -23,6 +23,7 @@ const CollectionSlider = ({ title, description, items }: CollectionSliderProps) 
             image={item.picture?.startsWith('https://acmucsd') ? item.picture : NoImage.src}
             title={item.itemName}
             href={`${config.itemRoute}${item.uuid}`}
+            cost={item.options[0]?.price ?? 0}
             key={item.uuid}
           />
         ))}
