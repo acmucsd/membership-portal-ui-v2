15f07862c9ba74558c2f94314ff0b6182698ed29
Andrew Smithwick <andy.smithwick@gmail.com>
2023-09-06 00:38:48 -0700

    Component now remembers your selection between reloads
    Now avoids "Hydration mismatch" (read here)
    
    https://www.npmjs.com/package/next-themes

--- src/components/common/ThreeToggle/index.tsx
+++ src/components/common/ThreeToggle/index.tsx
@@ -15,9 +15,10 @@ import styles from './style.module.scss';
 const ThreeToggle = () => {
   //   const { theme = 'light', onThemeChange } = props;
   const { theme, setTheme } = useTheme();
-  const [switchStyle, setSwitchStyle] = useState(styles.switchOne);
-  const [currAltText, setCurrAltText] = useState('Icon representing current theme');
-  const [currIcon, setCurrIcon] = useState('/assets/icons/moon.svg');
+  const [mounted, setMounted] = useState(false);
+  // const [switchStyle, setSwitchStyle] = useState(styles.switchOne);
+  // const [currAltText, setCurrAltText] = useState('Icon representing current theme');
+  // const [currIcon, setCurrIcon] = useState('/assets/icons/moon.svg');
 
   const lightId = `light${useId()}`;
   const systemId = `system${useId()}`;
@@ -27,28 +28,84 @@ const ThreeToggle = () => {
   // let currIcon = '/assets/icons/moon.svg';
   // let currAltText = 'Icon representing current theme';
 
-  useEffect(() => {
+  const calcStyle = (newTheme: string | undefined) => {
+    switch (newTheme) {
+      case 'light':
+        return styles.switchOne;
+      case 'system':
+        return styles.switchTwo;
+      case 'dark':
+        return styles.switchThree;
+      default:
+        console.log(newTheme);
+        return styles.switchOne;
+    }
+  };
+
+  const calcIcon = (theme: string | undefined) => {
     switch (theme) {
       case 'light':
-        setSwitchStyle(styles.switchOne);
-        setCurrIcon('/assets/icons/sun.svg');
-        setCurrAltText('Icon representing light theme is on');
-        break;
+        return '/assets/icons/sun.svg';
       case 'system':
-        setSwitchStyle(styles.switchTwo);
-        setCurrAltText('Icon representing system theme is on');
-        break;
+        return '/assets/icons/moon.svg';
       case 'dark':
-        setSwitchStyle(styles.switchThree);
-        setCurrAltText('Icon representing dark theme is on');
-        setCurrIcon('/assets/icons/moon.svg');
-        break;
+        return '/assets/icons/moon.svg';
       default:
-        setSwitchStyle(styles.switchOne);
-        console.log(theme);
-        break;
+        return '/assets/icons/moon.svg';
     }
-  }, [theme]);
+  };
+
+  const calcAltText = (theme: string | undefined) => {
+    switch (theme) {
+      case 'light':
+        return 'Icon representing light theme is on';
+      case 'system':
+        return 'Icon representing system theme is on';
+      case 'dark':
+        return 'Icon representing dark theme is on';
+      default:
+        return 'Icon representing dark theme is on';
+    }
+  };
+
+  // const switchStyle = useMemo(() => calcStyle(theme), [theme]);
+  // const currAltText = useMemo(() => calcAltText(theme), [theme]);
+  // const currIcon = useMemo(() => calcIcon(theme), [theme]);
+
+  const switchStyle = calcStyle(theme);
+  const currAltText = calcAltText(theme);
+  const currIcon = calcIcon(theme);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  // useEffect(() => {
+  //   switch (theme) {
+  //     case 'light':
+  //       setSwitchStyle(styles.switchOne);
+  //       setCurrIcon('/assets/icons/sun.svg');
+  //       setCurrAltText('Icon representing light theme is on');
+  //       break;
+  //     case 'system':
+  //       setSwitchStyle(styles.switchTwo);
+  //       setCurrAltText('Icon representing system theme is on');
+  //       break;
+  //     case 'dark':
+  //       setSwitchStyle(styles.switchThree);
+  //       setCurrAltText('Icon representing dark theme is on');
+  //       setCurrIcon('/assets/icons/moon.svg');
+  //       break;
+  //     default:
+  //       setSwitchStyle(styles.switchOne);
+  //       console.log(theme);
+  //       break;
+  //   }
+  // }, [theme]);
+
+  if (!mounted) {
+    return null;
+  }
 
   return (
     <form className={styles.switch}>
@@ -98,3 +155,4 @@ export default ThreeToggle;
 // Links for knowledge:
 // https://stackoverflow.com/questions/71039088/what-is-onchange-e-setnamee-target-value-in-react-mean
 // https://css-tricks.com/the-sass-ampersand/
+// https://www.npmjs.com/package/next-themes
