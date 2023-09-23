ad71e5781ecaf0ed0bcbed56a7d2675a24d8c00d
Sean <ussa.say@gmail.com>
2023-09-23 16:58:13 -0700

    Fetch leaderboard in selected time range

--- src/pages/leaderboard.tsx
+++ src/pages/leaderboard.tsx
@@ -3,13 +3,14 @@ import { config } from '@/lib';
 import { LeaderboardAPI } from '@/lib/api';
 import withAccessType from '@/lib/hoc/withAccessType';
 import { CookieService, PermissionService } from '@/lib/services';
+import { SlidingLeaderboardQueryParams } from '@/lib/types/apiRequests';
 import { PrivateProfile, PublicProfile } from '@/lib/types/apiResponses';
 import { CookieType } from '@/lib/types/enums';
 import { getProfilePicture, getUserRank } from '@/lib/utils';
 import MyPositionIcon from '@/public/assets/icons/my-position-icon.svg';
 import styles from '@/styles/pages/leaderboard.module.scss';
 import { GetServerSideProps } from 'next';
-import { useMemo, useRef, useState } from 'react';
+import { useEffect, useMemo, useRef, useState } from 'react';
 
 interface Match {
   index: number;
@@ -25,29 +26,65 @@ function filter<T extends PublicProfile>(users: T[], query: string): (T & { matc
 
 /** Year ACM was founded. */
 const START_YEAR = 2019;
+/** Number of seconds in a day. */
+const DAY_SECONDS = 86400;
 
 interface LeaderboardProps {
-  leaderboard: PublicProfile[];
+  authToken: string;
+  initLeaderboard: PublicProfile[];
   user: PrivateProfile;
 }
 
-const LeaderboardPage = ({ leaderboard, user: { uuid } }: LeaderboardProps) => {
+const LeaderboardPage = ({ authToken, initLeaderboard, user: { uuid } }: LeaderboardProps) => {
+  const [leaderboard, setLeaderboard] = useState(initLeaderboard);
+
   const [query, setQuery] = useState('');
   const myPosition = useRef<HTMLAnchorElement>(null);
 
-  const results = leaderboard.map((user, index) => ({ ...user, position: index + 1 }));
-  const topThreeUsers = query === '' ? filter(results.slice(0, 3), query) : [];
-  const leaderboardRows = filter(query === '' ? results.slice(3) : results, query);
-
   const endYear = useMemo(() => {
     const today = new Date();
-    return today.getMonth() < 8 ? today.getFullYear() : today.getFullYear() + 1;
+    // Arbitrarily start the next academic year in August
+    return today.getMonth() < 7 ? today.getFullYear() : today.getFullYear() + 1;
   }, []);
   const years = [];
   for (let year = START_YEAR + 1; year <= endYear; year += 1) {
     years.unshift({ value: String(year), label: `${year - 1}–${year}` });
   }
   const [sort, setSort] = useState<string>(String(endYear));
+  const [startTime, endTime] = useMemo((): [number, number] => {
+    const now = Date.now() / 1000;
+    switch (sort) {
+      case 'past-week':
+        return [now - DAY_SECONDS * 7, Infinity];
+      case 'past-month':
+        return [now - DAY_SECONDS * 28, Infinity];
+      case 'past-year':
+        return [now - DAY_SECONDS * 365, Infinity];
+      case 'all-time':
+        return [-Infinity, Infinity];
+      default: {
+        const year = +sort;
+        // Arbitrarily academic years on August 1, which should be during the summer
+        return [new Date(year - 1, 7, 1).getTime() / 1000, new Date(year, 7, 1).getTime() / 1000];
+      }
+    }
+  }, [sort]);
+  useEffect(() => {
+    const params: SlidingLeaderboardQueryParams = { limit: 0 };
+    // Passing `undefined` or `Infinity` directly to the object sends them as
+    // strings to the back end, which returns a 400 error
+    if (startTime !== -Infinity) {
+      params.from = startTime;
+    }
+    if (endTime !== Infinity) {
+      params.from = endTime;
+    }
+    LeaderboardAPI.getLeaderboard(authToken, params).then(setLeaderboard);
+  }, [authToken, startTime, endTime]);
+
+  const results = leaderboard.map((user, index) => ({ ...user, position: index + 1 }));
+  const topThreeUsers = query === '' ? filter(results.slice(0, 3), query) : [];
+  const leaderboardRows = filter(query === '' ? results.slice(3) : results, query);
 
   return (
     <div className={styles.container}>
@@ -135,14 +172,15 @@ export default LeaderboardPage;
 const getServerSidePropsFunc: GetServerSideProps = async ({ req, res }) => {
   const AUTH_TOKEN = CookieService.getServerCookie(CookieType.ACCESS_TOKEN, { req, res });
 
-  // Get all leaderboard items
-  const leaderboard = await LeaderboardAPI.getLeaderboard(AUTH_TOKEN, {
-    limit: 0,
+  // Only render first 100 leaderboard items on the back end
+  const initLeaderboard = await LeaderboardAPI.getLeaderboard(AUTH_TOKEN, {
+    limit: 100,
   });
 
   return {
     props: {
-      leaderboard,
+      authToken: AUTH_TOKEN,
+      initLeaderboard,
     },
   };
 };
