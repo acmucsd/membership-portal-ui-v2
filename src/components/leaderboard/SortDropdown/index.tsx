23f3fae242019d405b72dcadc9636852963638d3
Sean <ussa.say@gmail.com>
2023-09-23 18:41:31 -0700

    Fix keyboard <select> bugs

--- src/components/leaderboard/SortDropdown/index.tsx
+++ src/components/leaderboard/SortDropdown/index.tsx
@@ -12,6 +12,9 @@ interface SortDropdownProps {
   ariaLabel: string;
   options: (SortOption | '---')[];
   value: string;
+  // Justification for disabling rules: This seems to be a false positive.
+  // https://stackoverflow.com/q/63767199/
+  // eslint-disable-next-line no-unused-vars
   onChange: (value: string) => void;
 }
 
@@ -36,7 +39,20 @@ const SortDropdown = ({ name, ariaLabel, options, value, onChange }: SortDropdow
     // users. Only the fancy, inaccessible dropdown should be available for
     // mouse/touch users.
     // eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-static-element-interactions
-    <div className={styles.dropdownWrapper} aria-label={ariaLabel} onClick={() => setOpen(true)}>
+    <div
+      className={styles.dropdownWrapper}
+      aria-label={ariaLabel}
+      onClick={e => {
+        // Using the keyboard to select an option fires the click event on
+        // <select>; prevent it from opening the fake dropdown. The <select> has
+        // pointer-events: none so it shouldn't receive the click event any
+        // other way.
+        if (e.target instanceof HTMLElement && e.target.tagName === 'SELECT') {
+          return;
+        }
+        setOpen(true);
+      }}
+    >
       <select name={name} id={name} value={value} onChange={e => onChange(e.currentTarget.value)}>
         {options.map(option =>
           option !== '---' ? (
