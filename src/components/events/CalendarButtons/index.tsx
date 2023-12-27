a86489cef733c95e09bb0cd07fbec07430e9c6d9
Alex Zhang <alexzhang1618@gmail.com>
2023-12-26 22:27:31 -0800

    Rewrite calendar buttons into <a> elements

--- src/components/events/CalendarButtons/index.tsx
+++ src/components/events/CalendarButtons/index.tsx
@@ -1,23 +1,17 @@
+import { Typography } from '@/components/common';
 import { showToast } from '@/lib';
 import { PublicEvent } from '@/lib/types/apiResponses';
 import { formatURLEventTitle } from '@/lib/utils';
 import AppleCalendarLogo from '@/public/assets/icons/applecalendar.svg';
 import GoogleCalendarLogo from '@/public/assets/icons/googlecalendar.svg';
 import * as ics from 'ics';
+import { ReactNode } from 'react';
 import styles from './style.module.scss';
 
-// Referencing this answer to save text in a file and download it
-// https://stackoverflow.com/questions/44656610/download-a-string-as-txt-file-in-react
-const createDownloadFile = (textContent: string, title: string): void => {
-  const element = document.createElement('a');
-  const file = new Blob([textContent], { type: 'text/calendar' });
-  element.href = URL.createObjectURL(file);
-  element.download = `${title}.ics`;
-  document.body.appendChild(element);
-  element.click();
-};
-
-const saveToGoogleCal = ({ title, description, location, start, end }: PublicEvent): void => {
+/**
+ * Given event details, generate a URL that will draft a Google Calendar event.
+ */
+const generateGCalURL = ({ title, description, location, start, end }: PublicEvent): string => {
   const url = new URL('https://www.google.com/calendar/render?action=TEMPLATE');
   const params = new URLSearchParams(url.search);
   params.append('text', title);
@@ -27,11 +21,15 @@ const saveToGoogleCal = ({ title, description, location, start, end }: PublicEve
   const endISO = end.replaceAll('-', '').replaceAll(':', '').replaceAll('.', '');
   params.append('dates', `${startISO}/${endISO}`);
 
-  const GCAL_LINK = `${url.origin}${url.pathname}?${params.toString()}`;
-  window.open(GCAL_LINK);
+  const GCAL_URL = `${url.origin}${url.pathname}?${params.toString()}`;
+  return GCAL_URL;
 };
 
-const saveToAppleCal = (event: PublicEvent): void => {
+/**
+ * Generates an <a> component that downloads the Apple Calendar .ics file to the user's computer.
+ * If an error occurs during .ics file generation, shows an error toast instead.
+ */
+const generateAppleCalLink = (event: PublicEvent, children: ReactNode) => {
   const startDate = new Date(event.start);
   const endDate = new Date(event.end);
   const duration = endDate.getTime() - startDate.getTime();
@@ -54,10 +52,24 @@ const saveToAppleCal = (event: PublicEvent): void => {
 
   const response = ics.createEvent(attributes);
   if (response.value) {
-    createDownloadFile(response.value, formatURLEventTitle(event.title));
-  } else {
-    showToast(response.error?.message || 'An error occurred when saving to Apple Calendar.');
+    // Referencing this answer to save text in a file and download it
+    // https://stackoverflow.com/questions/44656610/download-a-string-as-txt-file-in-react
+    const file = new Blob([response.value], { type: 'text/calendar' });
+    const href = URL.createObjectURL(file);
+    const download = `${formatURLEventTitle(event.title)}.ics`;
+    return (
+      <a className={styles.calendarLink} href={href} download={download}>
+        {children}
+      </a>
+    );
   }
+  const errorMessage =
+    response.error?.message || 'An error occurred when saving to Apple Calendar.';
+  return (
+    <button type="button" className={styles.calendarLink} onClick={() => showToast(errorMessage)}>
+      {children}
+    </button>
+  );
 };
 
 interface CalendarButtonProps {
@@ -65,20 +77,20 @@ interface CalendarButtonProps {
 }
 
 const CalendarButtons = ({ event }: CalendarButtonProps) => {
+  const gCalURL = generateGCalURL(event);
   return (
     <div className={styles.options}>
-      <button
-        type="button"
-        className={styles.calendarButton}
-        onClick={() => saveToGoogleCal(event)}
-      >
+      <a className={styles.calendarLink} href={gCalURL} target="blank">
         <GoogleCalendarLogo alt="google calendar" />
-        <b>Add to Google Calendar</b>
-      </button>
-      <button type="button" className={styles.calendarButton} onClick={() => saveToAppleCal(event)}>
-        <AppleCalendarLogo alt="apple calendar" />
-        <b>Add to Apple Calendar</b>
-      </button>
+        <Typography variant="h6/bold">Add to Google Calendar</Typography>
+      </a>
+      {generateAppleCalLink(
+        event,
+        <>
+          <AppleCalendarLogo alt="apple calendar" />
+          <Typography variant="h6/bold">Add to Apple Calendar</Typography>
+        </>
+      )}
     </div>
   );
 };
--- src/components/events/CalendarButtons/style.module.scss.d.ts
+++ src/components/events/CalendarButtons/style.module.scss.d.ts
@@ -1,5 +1,6 @@
 export type Styles = {
   calendarButton: string;
+  calendarLink: string;
   options: string;
 };
 
