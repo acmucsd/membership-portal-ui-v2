b24f0893b2877fe79557306fbc3b13ccfafec37d
Alex Zhang <alexzhang1618@gmail.com>
2023-12-30 09:05:48 -0800

    Tiny cleanup

--- src/components/store/Diamonds/index.tsx
+++ src/components/store/Diamonds/index.tsx
@@ -13,11 +13,11 @@ const Diamonds = ({ count, discount, className }: DiamondsProps) => {
   const discountActive = discount !== undefined && discount < count;
   return (
     <span className={className}>
-      <span className={className} style={discountActive ? { textDecoration: 'line-through' } : {}}>
+      <span style={discountActive ? { textDecoration: 'line-through' } : {}}>
         {(count >= 1e5 ? compactFormat : numberFormat).format(count)}
       </span>
       {discountActive && (
-        <span className={className} style={{ color: 'var(--theme-danger-1)' }}>
+        <span style={{ color: 'var(--theme-danger-1)' }}>
           {' '}
           {(discount >= 1e5 ? compactFormat : numberFormat).format(discount)}
         </span>
