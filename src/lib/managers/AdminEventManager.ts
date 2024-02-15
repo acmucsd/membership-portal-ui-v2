dd7060bec6f69bb5bba0a935ebd0beeec7bb7a62
Alex Zhang <alexzhang1618@gmail.com>
2024-02-14 16:26:29 -0800

    Cleanup code

--- src/lib/managers/AdminEventManager.ts
+++ src/lib/managers/AdminEventManager.ts
@@ -84,11 +84,6 @@ interface EditEventRequest {
 
 export const editEvent = async (data: EditEventRequest & AuthAPIHandlerProps) => {
   const { onSuccessCallback, onFailCallback, token, event, uuid } = data;
-  onFailCallback?.(
-    new Error(
-      `${config.file.MAX_EVENT_COVER_SIZE_KB}, isProd: ${process.env.NEXT_PUBLIC_PRODUCTION}, node: ${process.env.NODE_ENV}`
-    )
-  );
   if (data.cover && data.cover.size > config.file.MAX_EVENT_COVER_SIZE_KB * 1024) {
     onFailCallback?.(new Error('Cover size too large'));
     return;
