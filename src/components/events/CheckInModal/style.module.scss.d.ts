943203b4e018cba8831167f1ba4617a159803ed1
Alex Zhang <alexzhang1618@gmail.com>
2024-02-21 17:55:01 -0800

    Add modal confetti

--- src/components/events/CheckInModal/style.module.scss.d.ts
+++ src/components/events/CheckInModal/style.module.scss.d.ts
@@ -1,11 +1,17 @@
 export type Styles = {
+  ai: string;
   close: string;
   container: string;
+  cyber: string;
+  design: string;
   done: string;
+  general: string;
   graphic: string;
+  hack: string;
   header: string;
   headerText: string;
   image: string;
+  innovate: string;
   subheader: string;
   subheaderText: string;
 };
