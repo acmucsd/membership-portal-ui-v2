7a98f87b941ec04c65c7b2aa7cf641b6ed2eb4a4
Alex Zhang <alexzhang1618@gmail.com>
2024-03-29 14:21:59 -0700

    Event modal tweaks

--- src/components/events/EventDetail/index.tsx
+++ src/components/events/EventDetail/index.tsx
@@ -1,12 +1,10 @@
 import { CommunityLogo, Typography } from '@/components/common';
 import CalendarButtons from '@/components/events/CalendarButtons';
-import PointsDisplay from '@/components/events/PointsDisplay';
+import EventBadges from '@/components/events/EventBadges';
 import { PublicEvent, PublicOrderPickupEvent } from '@/lib/types/apiResponses';
-import { fixUrl, formatEventDate, getDefaultEventCover, isOrderPickupEvent } from '@/lib/utils';
+import { formatEventDate, getDefaultEventCover, isOrderPickupEvent } from '@/lib/utils';
 import CloseIcon from '@/public/assets/icons/close-icon.svg';
-import LinkIcon from '@/public/assets/icons/link.svg';
 import Image from 'next/image';
-import Link from 'next/link';
 import styles from './style.module.scss';
 
 interface EventDetailProps {
@@ -15,18 +13,14 @@ interface EventDetailProps {
 }
 
 const EventDetail = ({ event, attended }: EventDetailProps) => {
-  const { cover, title, start, end, location, committee, pointValue, eventLink, description } =
-    isOrderPickupEvent(event)
-      ? {
-          ...(event.linkedEvent ?? {}),
-          ...event,
-        }
-      : event;
+  const { cover, title, start, end, location, committee, description } = isOrderPickupEvent(event)
+    ? {
+        ...(event.linkedEvent ?? {}),
+        ...event,
+      }
+    : event;
 
   const displayCover = getDefaultEventCover(cover);
-  const uuidForLink = isOrderPickupEvent(event) ? event.linkedEvent?.uuid : event.uuid;
-  const displayEventLink =
-    fixUrl(eventLink ?? '') || (uuidForLink && `https://acmucsd.com/events/${uuidForLink}`) || '';
   const isUpcomingEvent = new Date(start) > new Date();
 
   return (
@@ -35,13 +29,12 @@ const EventDetail = ({ event, attended }: EventDetailProps) => {
         <button type="submit" aria-label="Close" className={styles.close}>
           <CloseIcon aria-hidden className={styles.closeIcon} />
         </button>
-        {pointValue && <PointsDisplay points={pointValue} attended={attended} />}
         <Image src={displayCover} alt="Event Cover Image" style={{ objectFit: 'cover' }} fill />
       </div>
       <div className={styles.contents}>
         <div className={styles.header}>
           <div className={styles.eventDetails}>
-            <CommunityLogo community={committee ?? 'General'} size={100} />
+            <CommunityLogo community={committee ?? 'General'} size={140} />
             <div>
               <Typography
                 className={styles.eventTitle}
@@ -52,14 +45,15 @@ const EventDetail = ({ event, attended }: EventDetailProps) => {
               </Typography>
               <Typography
                 className={styles.eventInfo}
-                variant="title/medium"
+                variant="title/small"
                 suppressHydrationWarning
               >
                 {formatEventDate(start, end, true)}
               </Typography>
-              <Typography className={styles.eventInfo} variant="title/medium">
+              <Typography className={styles.eventInfo} variant="title/small">
                 {location}
               </Typography>
+              <EventBadges className={styles.badges} event={event} attended={attended} />
             </div>
           </div>
 
@@ -69,19 +63,6 @@ const EventDetail = ({ event, attended }: EventDetailProps) => {
         <Typography variant="body/medium" style={{ wordBreak: 'break-word' }}>
           {description}
         </Typography>
-        {displayEventLink && (
-          <Link className={styles.link} href={displayEventLink}>
-            <div style={{ width: 11 }}>
-              <LinkIcon aria-hidden />
-            </div>
-            <Typography
-              variant="body/medium"
-              style={{ color: 'var(--theme-primary-2)', wordBreak: 'break-all' }}
-            >
-              {displayEventLink}
-            </Typography>
-          </Link>
-        )}
       </div>
     </>
   );
--- src/components/events/EventDetail/style.module.scss.d.ts
+++ src/components/events/EventDetail/style.module.scss.d.ts
@@ -1,4 +1,5 @@
 export type Styles = {
+  badges: string;
   close: string;
   closeIcon: string;
   contents: string;
