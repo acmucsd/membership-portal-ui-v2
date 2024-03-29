7d9eaa4aaa400870faa6e90179876f8cd27e3dac
Alex Zhang <acz001@ucsd.edu>
2024-03-28 22:10:51 -0700

    Add Live tag to events, add placeholder for empty event carousels (#184)
    * Add Live tag to events, add placeholder for empty event carousels
    
    * Remove unnecessary CSS
    
    * Make carousel event cards mobile friendly
    
    * Fix styling

--- src/components/events/EventCarousel/index.tsx
+++ src/components/events/EventCarousel/index.tsx
@@ -1,6 +1,7 @@
 import { Carousel, Typography } from '@/components/common';
 import { config } from '@/lib';
 import { PublicAttendance, PublicEvent } from '@/lib/types/apiResponses';
+import DiamondFriends from '@/public/assets/graphics/portal/diamond-friends.svg';
 import Link from 'next/link';
 import EventCard from '../EventCard';
 import styles from './style.module.scss';
@@ -9,9 +10,10 @@ interface EventCarouselProps {
   title: string;
   events: PublicEvent[];
   attendances: PublicAttendance[];
+  placeholder: string;
 }
 
-const EventCarousel = ({ title, events, attendances }: EventCarouselProps) => {
+const EventCarousel = ({ title, events, attendances, placeholder }: EventCarouselProps) => {
   return (
     <div className={styles.wrapper}>
       <div className={styles.header}>
@@ -22,16 +24,23 @@ const EventCarousel = ({ title, events, attendances }: EventCarouselProps) => {
           See all events &gt;
         </Link>
       </div>
-      <Carousel>
-        {events.map(event => (
-          <EventCard
-            className={styles.card}
-            key={event.uuid}
-            event={event}
-            attended={attendances.some(a => a.event.uuid === event.uuid)}
-          />
-        ))}
-      </Carousel>
+      {events.length > 0 ? (
+        <Carousel>
+          {events.map(event => (
+            <EventCard
+              className={styles.card}
+              key={event.uuid}
+              event={event}
+              attended={attendances.some(a => a.event.uuid === event.uuid)}
+            />
+          ))}
+        </Carousel>
+      ) : (
+        <div className={styles.noEvents}>
+          <DiamondFriends />
+          <Typography variant="h4/regular">{placeholder}</Typography>
+        </div>
+      )}
     </div>
   );
 };
