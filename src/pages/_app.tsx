4020a81ba775f51e57333da9b70e571b1b50cd0c
Andrew Smithwick <andy.smithwick@gmail.com>
2024-03-30 14:34:42 -0700

    Synced theme transition time across elements (#194)
    * Synced theme transition time across elements
    
    * Made global transition 0.3s instead of snappy
    
    * Deleted old comments
    
    * Fixed body not animating properly
    
    * Delete style.module.scss

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -18,6 +18,7 @@ export default function MyApp({ Component, pageProps }: AppProps) {
     <>
       <style jsx global>{`
         * {
+          transition: all ease;
           font-family: ${dmSans.style.fontFamily}, sans-serif;
         }
       `}</style>
