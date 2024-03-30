6c0dbe22b8a943b2ef136a2b775c1c514d86f05e
Sean <ussa.say@gmail.com>
2024-03-29 23:22:13 -0700

    Link to feedback page from event modal

--- src/components/events/EventModal/index.tsx
+++ src/components/events/EventModal/index.tsx
@@ -1,6 +1,9 @@
 import { Modal } from '@/components/common';
 import EventDetail from '@/components/events/EventDetail';
+import { config } from '@/lib';
 import { PublicEvent } from '@/lib/types/apiResponses';
+import Link from 'next/link';
+import { useMemo } from 'react';
 
 interface EventModalProps {
   open: boolean;
@@ -10,9 +13,19 @@ interface EventModalProps {
 }
 
 const EventModal = ({ open, attended, event, onClose }: EventModalProps) => {
+  const started = useMemo(() => new Date() >= new Date(event.start), [event.start]);
+
   return (
     <Modal open={open} onClose={onClose} bottomSheet>
       <EventDetail event={event} attended={attended} showCloseBtn />
+      {started ? (
+        <Link
+          href={`${config.eventsRoute}/${event.uuid}`}
+          style={{ color: 'var(--theme-blue-text-button)' }}
+        >
+          Give feedback
+        </Link>
+      ) : null}
     </Modal>
   );
 };
