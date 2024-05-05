8219c00ea06df0889234fa4718ca258a9b91189c
Sean <ussa.say@gmail.com>
2024-05-04 22:09:12 -0800

    Admin view resumes page (#248)
    * Admin view resumes page
    
    * Add pagination
    
    * Zip all resumes

---
+++ src/components/admin/Resume/index.tsx
@@ -0,0 +1,46 @@
+import { GifSafeImage, Typography } from '@/components/common';
+import { config } from '@/lib';
+import { PublicResume } from '@/lib/types/apiResponses';
+import { getFileName, getProfilePicture } from '@/lib/utils';
+import Link from 'next/link';
+import { BsDownload } from 'react-icons/bs';
+import styles from './style.module.scss';
+
+interface ResumeProps {
+  resume: PublicResume;
+}
+
+const Resume = ({ resume }: ResumeProps) => {
+  const fileName = getFileName(resume.url, 'resume.pdf');
+
+  return (
+    <div className={styles.wrapper}>
+      {resume.user ? (
+        <Link href={`${config.userProfileRoute}${resume.user.handle}`} className={styles.user}>
+          <GifSafeImage
+            src={getProfilePicture(resume.user)}
+            width={48}
+            height={48}
+            alt={`Profile picture for ${resume.user.firstName} ${resume.user.lastName}`}
+            className={styles.image}
+          />
+          <Typography variant="label/large" className={styles.name}>
+            {resume.user.firstName} {resume.user.lastName}
+          </Typography>
+          <p className={styles.info}>
+            {resume.user.major} ({resume.user.graduationYear})
+          </p>
+        </Link>
+      ) : null}
+      <Link href={resume.url} className={styles.resume}>
+        <BsDownload className={styles.image} />
+        <p className={styles.name}>{fileName}</p>
+        <p className={styles.info}>
+          Uploaded {new Date(resume.lastUpdated).toLocaleDateString('en-US', { dateStyle: 'full' })}
+        </p>
+      </Link>
+    </div>
+  );
+};
+
+export default Resume;
