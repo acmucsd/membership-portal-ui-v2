2d5b880b759a1dd2b6dc7d4e309828b7c8e79456
Sean <ussa.say@gmail.com>
2024-05-13 19:11:26 -0700

    always show fulfilled items

--- src/components/store/OrderSummary/index.tsx
+++ src/components/store/OrderSummary/index.tsx
@@ -34,19 +34,9 @@ const isOrderActionable = (status: OrderStatus, pickupEvent: PublicOrderPickupEv
 
 interface OrderItemPreviewProps {
   item: OrderItemQuantity;
-  orderStatus: OrderStatus;
 }
 
-const OrderItemPreview = ({ item, orderStatus }: OrderItemPreviewProps) => {
-  if (item.fulfilled && orderStatus === OrderStatus.PLACED) {
-    /*
-     * When a partially fulfilled order is rescheduled,
-     * the status is changed to PLACED, but all items remain (some as fulfilled).
-     * These items should be hidden.
-     */
-    return null;
-  }
-
+const OrderItemPreview = ({ item }: OrderItemPreviewProps) => {
   return (
     <Link href={`${config.store.itemRoute}${item.option.item.uuid}`} className={styles.itemInfo}>
       <div className={styles.image}>
@@ -87,34 +77,45 @@ interface OrderItemsSummaryProps {
 }
 
 const OrderItemsSummary = ({ items, orderStatus }: OrderItemsSummaryProps) => {
-  if (orderStatus !== OrderStatus.PARTIALLY_FULFILLED) {
+  const pickedUpItems = items.filter(item => item.fulfilled);
+  const notPickedUpItems = items.filter(item => !item.fulfilled);
+
+  if (pickedUpItems.length === 0 || notPickedUpItems.length === 0) {
     return (
       <>
         {items.map(item => (
-          <OrderItemPreview item={item} orderStatus={orderStatus} key={item.uuids[0]} />
+          <OrderItemPreview item={item} key={item.uuids[0]} />
         ))}
       </>
     );
   }
-  const pickedUpItems = items.filter(item => item.fulfilled);
-  const notPickedUpItems = items.filter(item => !item.fulfilled);
 
-  return (
-    <div>
+  const pickedUp = (
+    <>
       <Typography variant="h3/bold" className={styles.partiallyFulfilledText}>
-        Items Picked Up:
+        {orderStatus === OrderStatus.PLACED ? 'Already Picked Up:' : 'Items Picked Up:'}
       </Typography>
       {pickedUpItems.map(item => (
-        <OrderItemPreview item={item} orderStatus={orderStatus} key={item.uuids[0]} />
+        <OrderItemPreview item={item} key={item.uuids[0]} />
       ))}
-
-      <hr className={styles.divider} />
+    </>
+  );
+  const notPickedUp = (
+    <>
       <Typography variant="h3/bold" className={styles.partiallyFulfilledText}>
         Awaiting Pickup:
       </Typography>
       {notPickedUpItems.map(item => (
-        <OrderItemPreview item={item} orderStatus={orderStatus} key={item.uuids[0]} />
+        <OrderItemPreview item={item} key={item.uuids[0]} />
       ))}
+    </>
+  );
+
+  return (
+    <div>
+      {orderStatus === OrderStatus.PLACED ? notPickedUp : pickedUp}
+      <hr className={styles.divider} />
+      {orderStatus === OrderStatus.PLACED ? pickedUp : notPickedUp}
     </div>
   );
 };
@@ -161,45 +162,7 @@ const OrderSummary = ({
         onClose={() => setCancelModalOpen(false)}
         cancelOrder={cancelOrder}
       />
-      {items.map(item => (
-        <Link
-          href={`${config.store.itemRoute}${item.option.item.uuid}`}
-          key={item.uuids[0]}
-          className={styles.itemInfo}
-        >
-          <div className={styles.image}>
-            <Image
-              src={getDefaultOrderItemPhoto(item)}
-              style={{ objectFit: 'cover' }}
-              sizes="9.375rem"
-              alt="Store item picture"
-              fill
-            />
-          </div>
-          <div className={styles.itemSummary}>
-            <Typography variant="h4/bold">{item.option.item.itemName}</Typography>
-            <div className={styles.label}>
-              <Typography variant="h5/bold">Price: </Typography>
-              <Diamonds
-                count={item.option.price * item.quantity}
-                discount={item.salePriceAtPurchase}
-              />
-            </div>
-            <div className={styles.label}>
-              <Typography variant="h5/bold">Quantity: </Typography>
-              <Typography variant="h5/regular">{item.quantity}</Typography>
-            </div>
-            {item.option.metadata ? (
-              <div className={styles.label}>
-                <Typography variant="h5/bold">{`${capitalize(
-                  item.option.metadata.type
-                )}: `}</Typography>
-                <Typography variant="h5/regular">{item.option.metadata.value}</Typography>
-              </div>
-            ) : null}
-          </div>
-        </Link>
-      ))}
+      <OrderItemsSummary items={items} orderStatus={orderStatus} />
       <hr className={styles.divider} />
       <div className={styles.footer}>
         <div className={styles.buttons}>
