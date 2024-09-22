551e9bdbf2738cb09421a50b893fbb7f13fe856c
Sean <ussa.say@gmail.com>
2024-09-22 16:48:46 -0700

    temporary images for intro

--- src/components/onboarding/Intro/index.tsx
+++ src/components/onboarding/Intro/index.tsx
@@ -1,5 +1,29 @@
+import TempImage from '@/public/assets/graphics/cat404.png';
+import Image from 'next/image';
+import styles from './style.module.scss';
+
 const Intro = () => {
-  return <p>hey</p>;
+  return (
+    <div className={styles.wrapper}>
+      <Image
+        src={TempImage}
+        alt="temp image"
+        width={256}
+        height={256}
+        className={styles.image}
+        style={{ animationDelay: '0.2s' }}
+      />
+      <Image src={TempImage} alt="temp image" width={256} height={256} className={styles.image} />
+      <Image
+        src={TempImage}
+        alt="temp image"
+        width={256}
+        height={256}
+        className={styles.image}
+        style={{ animationDelay: '0.4s' }}
+      />
+    </div>
+  );
 };
 
 export default Intro;
